81a1f6ffafd622fc6ea55eeeca40d61642f88e1f
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-17 14:43:26 +0000

    Done.

--- index.js
+++ index.js
@@ -2,19 +2,7 @@
 
 function findMinAndRemoveSorted(array)
 {
-  // We shouldn't trust that array is sorted.
-  let min = array[0];
-  let minIndex = 0;
-
-  for(let i = 0; i < array.length; i++){
-    if(array[i] < min)
-    {
-      min = array[i]
-      minIndex = i
-    }
-  }
-  array.splice(minIndex, 1);
-  return min;
+  return array.shift();
 }
 
 // console.log(merge(
