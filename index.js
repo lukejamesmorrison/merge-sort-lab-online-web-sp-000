96405ba80458b7da06cb20bc77546155549370d5
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-17 13:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ function findMinAndRemoveSorted(array)
   let min = array[0];
   let minIndex = 0;
 
-  for(let i = 0; i < array.length; i++)
-  {
-    if(array[i] < min)
-    {
-      min = array[i];
-      minIndex = i;
-    };
-    array.splice(minIndex, 1);
-    return min;
-  }
+  array.splice(0, 1);
+  return min;
 }
