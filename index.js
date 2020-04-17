914d0a94b5d4dd02e611b812acd738826cb2b979
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-17 13:52:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,10 +24,8 @@ function merge(firstArray, secondArray)
     if(firstArray[0] < secondArray[0] || firstArray[0] === secondArray[0])
     {
       var currentMin = findMinAndRemoveSorted(firstArray);
-      console.log(currentMin);
     } else {
       var currentMin = findMinAndRemoveSorted(secondArray);
-      console.log(currentMin);
     };
     sorted.push(currentMin);
   }
