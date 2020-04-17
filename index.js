ef967537741bdcbcb7d2c2e933c5b0cd76ff22dc
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-17 13:34:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ let array = [2,3,4];
 function findMinAndRemoveSorted(array)
 {
   let min = array[0];
-  let minIndex = 0;
-
   array.splice(0, 1);
   return min;
+}
+
+function merge(firstArray, secondArray)
+{
+
 }
