b0142b0ac23cc5ab06667cc8a092bdd24b751323
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-17 13:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 
+let array = [2,4,5,1];
+
+findMinAndRemoveSorted(array);
+
 function findMinAndRemoveSorted(array)
 {
+  let min = array[0];
+  let minIndex = 0;
 
+  for(let i = 0; i < array.length; i++)
+  {
+    if(array[0] < min)
+    {
+      min = array[0];
+      minIndex = i;
+    };
+    array.splice(minIndex, 1);
+    return min;
+  }
 }
